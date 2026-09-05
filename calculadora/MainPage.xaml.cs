acd1490d6facecc198cf74eb6278ca905cfea323
Marcelo-Slv <marceloexpedito13@gmail.com>
2026-09-04 22:33:58 -0300

    Atualiza calculadora de gorjeta

--- calculadora/MainPage.xaml.cs
+++ calculadora/MainPage.xaml.cs
@@ -2,16 +2,9 @@
 {
     public partial class MainPage : ContentPage
     {
-        private double _total;
-
         public MainPage()
         {
             InitializeComponent();
-        }
-
-        private void OnBillChanged(object? sender, TextChangedEventArgs e)
-        {
-            CalcularGorjeta();
         }
 
         private void Gorjeta15PorcentoButton_Clicked(object? sender, EventArgs e)
@@ -26,31 +19,64 @@
 
         private void ArredondarParaCimaButton_Clicked(object? sender, EventArgs e)
         {
-            ValorTotaldaCompraLabel.Text = Math.Ceiling(_total).ToString("F2");
+
         }
 
         private void ArredondarParaBaixoButton_Clicked(object? sender, EventArgs e)
         {
-            ValorTotaldaCompraLabel.Text = Math.Floor(_total).ToString("F2");
+
+            PorcentagemGorjetaLabel.Text = $"{Math.Round(PorcentagemGorjetaSlider.Value, 1)} %";
+            double valorDaConta = Convert.ToDouble(ContaEntry.Text);
+            double porcentagemDaGorjeta = PorcentagemGorjetaSlider.Value / 100;
+            double valorDaGorjeta = valorDaConta * porcentagemDaGorjeta;
+            GorjetaLabel.Text = Convert.ToString(valorDaGorjeta);
+            double ValorTotal = valorDaConta + valorDaGorjeta;
+            ValorTotalTeste.Text = ValorTotal.ToString();
+
+            double ValorGorjeta = Convert.ToDouble(GorjetaLabel.Text);
+            double ValorTotalMesmo = Convert.ToDouble(GorjetaLabel.Text) + Convert.ToDouble(ValorTotalTeste.Text);
+            GorjetaLabel.Text = Math.Floor(ValorGorjeta).ToString();
+
+            ValorTotalLabel.Text = Convert.ToString(ValorTotalMesmo);
+
+
+
+
+
+
         }
 
-        private void CalcularGorjeta()
-        {
-            if (double.TryParse(billInput.Text, out double conta))
-            {
-                double percentual = PorcentagemGorjetaSlider.Value;
-                double gorjeta = conta * (percentual / 100);
-                _total = conta + gorjeta;
 
-                tipOutput.Text = gorjeta.ToString("F2");
-                ValorTotaldaCompraLabel.Text = _total.ToString("F2");
-            }
-        }
 
         private void PorcentagemGorjetaSlider_ValueChanged(object? sender, ValueChangedEventArgs e)
         {
-            tipPercent.Text = $"{Math.Round(e.NewValue)}%";
-            CalcularGorjeta();
+
+            PorcentagemGorjetaLabel.Text = $"{Math.Round(PorcentagemGorjetaSlider.Value, 1)} %";
+            double valorDaConta = Convert.ToDouble(ContaEntry.Text);
+            double porcentagemDaGorjeta = PorcentagemGorjetaSlider.Value / 100;
+            double valorDaGorjeta = valorDaConta * porcentagemDaGorjeta;
+            GorjetaLabel.Text = Convert.ToString(valorDaGorjeta);
+            double ValorTotal = valorDaConta + valorDaGorjeta;
+            ValorTotalLabel.Text = ValorTotal.ToString();
+
+
+
+
+
+
+
+            /*
+            PorcentagemGorjetaLabel.Text = $"{e.NewValue} %";
+
+            double valorDaConta = Convert.ToDouble(ContaEntry.Text);
+            double valorDaGorjeta = valorDaConta * (e.NewValue / 100);
+            GorjetaLabel.Text = valorDaGorjeta.ToString("F2");
+
+            double valorTotal = valorDaConta + valorDaGorjeta;
+            ValorTotalLabel.Text = valorTotal.ToString("F2");*/
+
+
+
         }
     }
 }
